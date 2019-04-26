99fa0fd7dd0d3e95e24f0064b45ccdfd2c2a96f9
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-04-26 07:33:41 -0400

    Adding upload image feature

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,7 +6,15 @@ class UserController {
   }
 
   async store (req, res) {
-    await User.create(req.body)
+    // pega o filename de req.file e renomeia e cria uma const chamada avatar
+    const { filename: avatar } = req.file
+
+    console.log(req.body)
+
+    // é passado um objeto para o User.create
+    // com todo o resto da informação de req.body
+    // e adiciona um campo chamado avatar com valor armazenado no const avatar
+    await User.create({ ...req.body, avatar })
 
     return res.redirect('/')
   }
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const multerConfig = require('./config/multer')
+const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
 const Usercontroller = require('./app/controllers/UserController')
 
 routes.get('/signup', Usercontroller.create)
-routes.post('/signup', Usercontroller.store)
+routes.post('/signup', upload.single('avatar'), Usercontroller.store)
 
 module.exports = routes
