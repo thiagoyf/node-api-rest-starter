392365f426091f2d873267d5db4eea508c4cea5c
thiagoyf <thiagoyudifukunaga@gmail.com>
2019-04-26 13:33:25 -0400

    Adding authentication

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,17 @@ const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
-const Usercontroller = require('./app/controllers/UserController')
+const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
 
-routes.get('/signup', Usercontroller.create)
-routes.post('/signup', upload.single('avatar'), Usercontroller.store)
+routes.get('/', SessionController.create)
+routes.post('/signin', SessionController.store)
+
+routes.get('/signup', UserController.create)
+routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.get('/app/dashboard', (req, res) => {
+  return res.render('dashboard')
+})
 
 module.exports = routes
